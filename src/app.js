33232a164fc77c3e18f9edf8eb7f68957cdda241
Nghia <hello.nghia22@gmail.com>
2021-01-15 04:12:09 -0500

    feat: Add event listener and handler

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+// Get input data from form
+const submitBtn = document.getElementById('btn')
+
+// Listener
+submitBtn.addEventListener('click', function() {
+    const name = document.getElementById('name').value
+    const feet = parseInt(document.getElementById('feet').value)
+    const inches = parseInt(document.getElementById('inches').value)
+    const weight = document.getElementById('weight').value
+    const diet = document.getElementById('diet').value
+
+    logToConsole(name,feet,inches,weight,diet)
+})
+
+// Helper functions
+function logToConsole(...rest) {
+    console.log(rest)
+}
+
+
+
+function feetToInches(ft) {
+
+}
+
 
 
 // Create Dino Constructor
@@ -14,3 +39,7 @@
 // Add tiles to DOM
 // Remove form from screen
 // On button click, prepare and display infographic
+
+
+
+
